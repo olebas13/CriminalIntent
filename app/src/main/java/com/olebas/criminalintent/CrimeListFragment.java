c8320ad2a2d328f7ba10752d2a584dc9be132da8
olebas13 <o.nevoyt13@gmail.com>
2018-09-20 16:46:40 +0300

    add intent and fix fragment crime

--- app/src/main/java/com/olebas/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/olebas/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.olebas.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -66,7 +67,8 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(getActivity(), mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(getActivity(), CrimeActivity.class);
+            startActivity(intent);
         }
     }
 
